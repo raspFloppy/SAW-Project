d22426c5b8b59c7205707cd87cd424fe61573285
raspFloppy <raspfloppy@protonmail.com>
2024-12-15 16:07:40 +0100

    Frontend routing implemented

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,8 @@
 <template>
-  <div id="app">
-    <input type="text" v-model="message" placeholder="Enter a message">
-    <button @click="sendMessage">Send Message</button>
-  </div>
+  <RouterLink to="/">Home</RouterLink> |
+  <RouterLink to="/registration">Registration</RouterLink> |
+  <RouterLink to="/login">Login</RouterLink>
+  <main>
+    <RouterView />
+  </main>
 </template>
-
-<script>
-import axios from 'axios';
-
-export default {
-  data() {
-    return {
-      message: ''
-    };
-  },
-  methods: {
-    async sendMessage() {
-      try {
-        const response = await axios.post('http://localhost:8081/api.php', {
-          message: this.message
-        });
-
-        console.log(response.data);
-      } catch (error) {
-        console.error(error);
-      }
-    }
-  }
-};
-</script>
---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import { createMemoryHistory, createRouter } from 'vue-router'
+import Registration from '@/views/Registration.vue';
+import Login from '@/views/Login.vue';
+import Home from '@/views/Home.vue';
+import NotFound from '@/views/NotFound.vue';
+
+const routes = [
+  {path: '/', component: Home},
+  {path: '/registration', component: Registration},
+  {path: '/login', component: Login},
+  {path: '/notfound', component: NotFound},
+]
+
+const router = createRouter({
+  history: createMemoryHistory(),
+  routes
+})
+
+
+export default router
