7ad0ee3e3e5b9ead85c58dba4bbff98f36fb399a
simoha5 <zeusssimo@gmail.com>
2024-12-22 14:54:13 +0100

    home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,83 @@
 <template>
-    <h1>HOME</h1>
-</template>
+ <ul class="menu menu-vertical lg:menu-horizontal bg-base-200 rounded-box">
+  <li><a>HTML</a></li>
+  <li><a>CSS</a></li>
+  <li><a>JAVA SCRiPT</a></li>
+  <li><a>PYTHON</a></li>
+  <li><a>C</a></li>
+  <li><a>C++</a></li>
+  <li><a>....</a></li>
+
+</ul>
+
+    <div
+  class="hero min-h-screen"
+  style="background-image: url('./download.jpeg');">
+  <div class="hero-overlay bg-opacity-60"></div>
+  <div class="hero-content text-neutral-content text-center">
+    <div class="max-w-md">
+      <h1 class="mb-5 text-5xl font-bold">Start Learning Today</h1>
+      <p class="mb-5">
+        Explore tutorials, courses, and resources to build your skills in programming.
+      </p>
+      <button class="btn btn-primary" onclick="window.location.href = './registration';">Get Started</button>
+    </div>
+  </div>
+</div>
+<div class="flex w-full flex-col lg:flex-row">
+  <div class="card bg-base-300 rounded-box grid h-320 flex-grow place-items-center">
+    <div  style="padding:3%;">
+  <h1 style="font-size:100px;font-weight:7000;margin-top:-18px!important;">HTML</h1>
+  <p style="font-size:19px;">The language for building web pages</p>
+  <a href="..."  style="margin-bottom:2px;">Learn HTML</a><br>
+  <a style="position:relative;overflow:visible;margin-bottom:1px;" href="..." >Video Tutorial</a><br>
+  <a href="https://campus.w3schools.com/collections/certifications/products/html-certificate" target="_blank" class="w3-button ga-fp w3-block ws-pink ref-button ws-pink-hover" title="Add HTML Certification">Get Certified</a><br>
+
+</div>
+  </div>
+  <div class="divider lg:divider-horizontal">--></div>
+  <div class="card bg-base-300 rounded-box grid h-320 flex-grow place-items-center">
+    <div  style="padding:3%">
+  <div  style="padding:16px;">
+    <h1 style="font-size:50px;font-weight:700;">HTML EXAMPEL</h1>   
+    <div  style="height:300px">
+&lt;!DOCTYPE html&gt;<br>
+&lt;html&gt;<br>&lt;head&gt;<br>
+&lt;title&gt;HTML Tutorial&lt;/title&gt;<br>&lt;/head&gt;<br>
+&lt;body&gt;<br><br>
+&lt;h1&gt;This is a heading&lt;/h1&gt;<br>
+&lt;p&gt;This is a paragraph.&lt;/p&gt;<br><br>
+&lt;/body&gt;<br>
+&lt;/html&gt;
+   </div>
+   <a href="/html/tryit.asp?filename=tryhtml_default_default" target="_blank" class="w3-button ga-fp tryit-button">Try it Yourself</a>
+  </div>
+ </div>
+  </div>
+</div>
+
+
+
+ 
+
+
+<footer class="footer bg-neutral text-neutral-content p-10">
+  <nav>
+    <h6 class="footer-title">Services</h6>
+    <a class="link link-hover">coding</a>
+    <a class="link link-hover">Design</a>
+  </nav>
+  <nav>
+    <h6 class="footer-title">Company</h6>
+    <a class="link link-hover">About us</a>
+    <a class="link link-hover">Contact</a>
+  </nav>
+  <nav>
+    <h6 class="footer-title">Legal</h6>
+    <a class="link link-hover">Privacy policy</a>
+    <a class="link link-hover">Cookie policy</a>
+  </nav>
+</footer>
+  </template>
+  
+ 
